9edacc5828f8b1213e95dcf5c4bdd16bdcc943ec
phamjhungf <herobine666666666@gmail.com>
2023-09-25 19:18:50 +0700

    Mega big update

---
+++ script.js
@@ -0,0 +1,14 @@
+alert('ko có  j ở đây 😭 ')
+function tinhtuoi() {
+    var tuoinhap = document.getElementById("ageinput").value;
+
+    if(tuoinhap==""){
+        document.getElementById("vungchua").innerHTML = '<div class="alert alert-danger"> 🗣️ Không được để rỗng</div>'
+    }else if(tuoinhap<=0){
+        document.getElementById("vungchua").innerHTML = '<div class="alert alert-danger"> 🤬 Mày chưa đẻ mà đòi tao tính à</div>'
+    }else if(tuoinhap<=100){
+        document.getElementById("vungchua").innerHTML = '<div class="alert alert-success"> 🤖 Tuổi của bạn là: '+ tuoinhap+'</div>'
+    } else {
+        document.getElementById("vungchua").innerHTML = '<div class="alert alert-danger"> 🥶 Con lạy cụ, cụ có phải là người không vậy ?</div>';
+    }
+}
